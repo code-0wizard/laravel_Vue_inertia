3e57425a127613b70946e65951783057d3c69ca6
hataken <bb52119638@gmail.com>
2025-03-20 14:42:54 +0900

    #6

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -5,6 +5,8 @@
   <div>
     <header class="bg-indigo-500 text-white">
       <nav class="flex items-center justify-between p-4 max-w-screen-lgmx-auto">
+        <h1>{{ $page.props.auth.user }}</h1>
+
         <div class="space-x-6">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
