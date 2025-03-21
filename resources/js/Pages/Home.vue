7740fbf5dc3551fddea7b7e5d74a8d256fafb3b9
hataken <bb52119638@gmail.com>
2025-03-22 00:34:01 +0900

    #15(途中)

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,10 @@
 <script setup>
+defineProps({
+  users: Object,
+})
 </script>
 
 <template>
-  <!-- <Head :title="` | ${page.component}`" /> -->
   <Head :title="$page.component" />
-  <!-- <h1>{{ $page.props.auth.user }}</h1> -->
-  <!-- <Link class="mt-[600px] block" href="/" preserve-scroll>Refresh</Link> -->
   <h1>Home page</h1>
 </template>
